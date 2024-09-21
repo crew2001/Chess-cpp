937c613ba6f0379ca6892f39a6905d5caef13f70
Cillian Rew <cillian.rew@gmail.com>
2024-09-21 11:10:54 +0100

    Fix bugs from clion refactoring

--- game_engine/eventmanager.cpp
+++ game_engine/eventmanager.cpp
@@ -11,26 +11,26 @@
  * @param turn the current turn of the game (even for white, odd for black)
  */
 void EventManager::HandleEvent(sf::Event &event, int turn) {
-  if (EventManager::CheckMate(-1)) {
-    cout << "CHECKMATE, white wins" << endl;
-    //        this->window->close();
-  }
-  if (EventManager::CheckMate(1)) {
-    cout << "CHECKMATE, black wins" << endl;
-    //        this->window->close();
-  }
-  //    x is col and y is row
-  int xpos = event.mouseButton.x;
-  int ypos = event.mouseButton.y;
-  row = static_cast<int>(static_cast<double>(abs(ypos)) * (8. / 1000.));
-  col = static_cast<int>(static_cast<double>(abs(xpos)) * (8. / 1000.));
-  PieceChosen = Board::board[row][col];
-  if ((turn % 2 == 0 && PieceChosen < 0) ||
-      (turn % 2 == 1 && PieceChosen > 0)) {
-    CorrectPiece = false;
-  } else {
-    CorrectPiece = true;
-  }
+    if (EventManager::CheckMate(-1)) {
+        cout << "CHECKMATE, white wins" << endl;
+        //        this->window->close();
+    }
+    if (EventManager::CheckMate(1)) {
+        cout << "CHECKMATE, black wins" << endl;
+        //        this->window->close();
+    }
+    //    x is col and y is row
+    int xpos = event.mouseButton.x;
+    int ypos = event.mouseButton.y;
+    row = static_cast<int>(static_cast<double>(abs(ypos)) * (8. / 1000.));
+    col = static_cast<int>(static_cast<double>(abs(xpos)) * (8. / 1000.));
+    PieceChosen = Board::board[row][col];
+    if ((turn % 2 == 0 && PieceChosen < 0) ||
+        (turn % 2 == 1 && PieceChosen > 0)) {
+        CorrectPiece = false;
+    } else {
+        CorrectPiece = true;
+    }
 }
 
 /**
@@ -43,63 +43,61 @@ void EventManager::HandleEvent(sf::Event &event, int turn) {
 void EventManager::HandleEvent(sf::Event &event,
                                vector<pair<int, int>> &gameMoves,
                                const pair<int, int> &currentPiece) {
-  int xpos = event.mouseButton.x;
-  int ypos = event.mouseButton.y;
-  row = static_cast<int>(static_cast<double>(abs(ypos)) * (8. / 1000.));
-  col = static_cast<int>(static_cast<double>(abs(xpos)) * (8. / 1000.));
-  //    PieceChosen = (m_board.getBoard())[row][col];
-  pair<int, int> searchVect = {row, col};
-  if (ranges::find(gameMoves, searchVect) != gameMoves.end()) {
-    if (Rules::CanMove(currentPiece, {row, col})) {
-      gameMoves = {{currentPiece}, {row, col}};
+    int xpos = event.mouseButton.x;
+    int ypos = event.mouseButton.y;
+    row = static_cast<int>(static_cast<double>(abs(ypos)) * (8. / 1000.));
+    col = static_cast<int>(static_cast<double>(abs(xpos)) * (8. / 1000.));
+    //    PieceChosen = (m_board.getBoard())[row][col];
+    pair<int, int> searchVect = {row, col};
+    if (ranges::find(gameMoves, searchVect) != gameMoves.end()) {
+        if (Rules::CanMove(currentPiece, {row, col})) {
+            gameMoves = {{currentPiece}, {row, col}};
+        } else {
+            gameMoves = {{}};
+        }
     } else {
-      gameMoves = {{}};
+        gameMoves = {{}};
     }
-  } else {
-    gameMoves = {{}};
-  }
 }
 
 void EventManager::GetMoves() {
-  Rules rules(PieceChosen, row, col);
-  m_available = rules.GetMoves();
+    Rules rules(PieceChosen, row, col);
+    m_available = rules.GetMoves();
 }
 
 void EventManager::printBoard() {
-  for (int i = 0; i < 8; i++) {
-    for (int j = 0; j < 8; j++) {
-      cout << arrboard[i][j] << " , ";
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            cout << arrboard[i][j] << " , ";
+        }
+        cout << endl;
     }
-    cout << endl;
-  }
 }
 
 bool EventManager::CheckMate(int sideSign) {
-  // * CHECK IF THE LAST PIECE WAS PAWN OR KNIGHT
-  // * IF SO THEN KING HAS TO MOVE/ATTACKING PIECE NEEDS TO BE TAKEN
-  if (!Rules::KingCheck(sideSign, Board::board)) {
-    return false;
-  }
-  int attacking = Board::lastPiece;
-  pair<int, int> kingUnderAttack =
-      attacking < 0 ? Board::findKing(1) : Board::findKing(-1);
-  Rules kings(Board::board[kingUnderAttack.first][kingUnderAttack.second],
-              kingUnderAttack.first, kingUnderAttack.second);
-  if (!kings.GetMoves().empty())
-    return false;
-  vector<pair<int, int>> piecesOnBoard;
-  for (int i = 0; i < 8; i++) {
-    for (int j = 0; j < 8; j++) {
-      if (Board::board[i][j] * attacking < 0) {
-        piecesOnBoard.emplace_back(i, j);
-      }
+    // * CHECK IF THE LAST PIECE WAS PAWN OR KNIGHT
+    // * IF SO THEN KING HAS TO MOVE/ATTACKING PIECE NEEDS TO BE TAKEN
+    if (!Rules::KingCheck(sideSign, Board::board)) {
+        return false;
     }
-  }
-  for (pair<int, int> protector : piecesOnBoard) {
-    Rules possibles(Board::board[protector.first][protector.second],
-                    protector.first, protector.second);
-    if (!possibles.GetMoves().empty())
-      return false;
-  }
-  return true;
+    int attacking = Board::lastPiece;
+    pair<int, int> kingUnderAttack =
+        attacking < 0 ? Board::findKing(1) : Board::findKing(-1);
+    Rules kings(Board::board[kingUnderAttack.first][kingUnderAttack.second],
+                kingUnderAttack.first, kingUnderAttack.second);
+    if (!kings.GetMoves().empty()) return false;
+    vector<pair<int, int>> piecesOnBoard;
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (Board::board[i][j] * attacking < 0) {
+                piecesOnBoard.emplace_back(i, j);
+            }
+        }
+    }
+    for (pair<int, int> protector : piecesOnBoard) {
+        Rules possibles(Board::board[protector.first][protector.second],
+                        protector.first, protector.second);
+        if (!possibles.GetMoves().empty()) return false;
+    }
+    return true;
 }
--- game_engine/game.h
+++ game_engine/game.h
@@ -5,55 +5,54 @@
 #pragma once
 
 #include "board.h"
-#include <vector>
 #include <array>
-#include <iostream>
+#include <vector>
+// #include <iostream>
+#include "eventmanager.h"
+#include "pieces.h"
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
-#include "pieces.h"
-#include "eventmanager.h"
-#include "rules.h"
+// #include "rules.h"
 using namespace std;
-class Game
-{
+class Game {
 public:
-    Game();
-    ~Game() = default;
+  Game();
+  ~Game() = default;
 
-    //    Accessors
+  //    Accessors
 
-    //  Modifiers
+  //  Modifiers
 
-    //  Functions
-    bool running() const { return this->window->isOpen(); }
-    void update();
-    void render();
-    void pollEvents();
-    void DrawBackground();
-    void Draw(sf::Drawable &l_drawable);
-    void ShowMoves();
-    void MakeMove();
-    void DrawCheckNotification();
+  //  Functions
+  bool running() const { return this->window->isOpen(); }
+  void update();
+  void render();
+  void pollEvents();
+  void DrawBackground();
+  void Draw(sf::Drawable &l_drawable);
+  void ShowMoves();
+  void MakeMove();
+  void DrawCheckNotification();
 
 private:
-    int turn;
-    //    true if white is in check
-    static bool whiteCheck;
-    //  true if black is in check
-    static bool blackCheck;
-    vector<pair<int, int>> canMove;
-    pair<int, int> firstClick;
-    sf::Event event;
-    vector<sf::Texture> m_textures = Pieces::loadTextures();
-    sf::RenderWindow *window;
-    sf::VideoMode videoMode;
-    EventManager m_evman;
-    vector<sf::Sprite> sprites;
-    Board m_board;
-    array<array<int, 8>, 8> arrboard = Board::board;
-    bool endGame;
-    void initVariables();
-    void initWindow();
-    void PiecesOnBoard();
-    void DrawPieces();
+  int turn;
+  //    true if white is in check
+  static bool whiteCheck;
+  //  true if black is in check
+  static bool blackCheck;
+  vector<pair<int, int>> canMove;
+  pair<int, int> firstClick;
+  sf::Event event;
+  vector<sf::Texture> m_textures = Pieces::loadTextures();
+  sf::RenderWindow *window;
+  sf::VideoMode videoMode;
+  EventManager m_evman;
+  vector<sf::Sprite> sprites;
+  Board m_board;
+  array<array<int, 8>, 8> arrboard = Board::board;
+  bool endGame;
+  void initVariables();
+  void initWindow();
+  void PiecesOnBoard();
+  void DrawPieces();
 };
